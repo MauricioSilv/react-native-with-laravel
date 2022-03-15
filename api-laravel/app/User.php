06c146cbbcf3a99aeabf18ff9ed1a1e6de85e5ee
mausilva828 <mausilva828@gmail.com>
2022-03-14 22:04:05 -0300

    Ajustes

--- api-laravel/app/User.php
+++ api-laravel/app/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
